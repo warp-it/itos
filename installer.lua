f9f62ee75b689f3ad91bd14245b573066e18c20a
Mikhail Yurchenko <polsit@yandex.ru>
2022-08-21 00:58:43 +0300

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -3,6 +3,11 @@ local term = require("term")
 local os = require("os")
 local filesystem = require("filesystem")
 local inet = require("internet")
+local computer = require("computer")
+
+if computer.getArchitecture() ~= "Lua 5.3" then
+    computer.setArchitecture("Lua 5.3")
+end
 
 local filesystems = {}
 for address, _ in component.list("filesystem") do table.insert(filesystems, component.proxy(address)) end
@@ -14,8 +19,6 @@ print("")
 local selection = tonumber(term.read())
 local fs = filesystems[selection]
 if fs == nil then print("Неправильный номер диска") os.exit() end
-
-filesystem.mount(fs, "/mnt/itos")
 
 local function write(file, content)
     local handle = fs.open(file, "w")
@@ -35,7 +38,25 @@ local function git(file)
     return request("https://raw.githubusercontent.com/warp-it/itos/main/"..file)
 end
 
-local files = git("files")
-for line in files:gmatch("[^\r\n]+") do
-
+local function processList(address, callback)
+    local list = git(address)
+    for line in list:gmatch("[^\r\n]+") do callback(line) end
 end
+
+print("Запись системы на диск "..fs.address)
+print("Форматирование диска..")
+
+local listFiles = fs.list("/")
+if listFiles ~= nil then for i = 1, #listFiles do fs.remove(listFiles[i]) end end
+
+processList("folders", function(line)
+    print("Создание директории "..line)
+    fs.makeDirectory(line)
+end)
+
+processList("files", function(line)
+    print("Запись файла "..line)
+    write(line, git(line))
+end)
+
+computer.shutdown(true)
